c211b2968bcc249acf46e136ec8830eb6db704fb
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2026-01-23 18:44:06 +0900

    Delete ccw directory

--- ccw/pr/pr_management.go
+++
@@ -1,8 +0,0 @@
-package pr
-
-// This file now serves as the main entry point for PR management operations.
-// The functionality has been split into separate files:
-// - pr_utilities.go: Core PRManager struct and utility functions
-// - pr_creation.go: PR creation operations
-// - ci_monitoring.go: CI monitoring and check operations
-// - comment_analysis.go: PR comment analysis operations
